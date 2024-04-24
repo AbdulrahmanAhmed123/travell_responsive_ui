dc6b5efd80c9d790430fbcb0a99bb495f3087113
abdoahmed <abdoahmed22231ad@gmail>
2024-04-25 01:59:11 +0200

    add style app

---
+++ lib/utils/colorsapp.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  ColorsApp._();
+
+  static const Color e1eColor = Color(0xff1E1E1E);
+  static const Color a0Color = Color(0xff008FA0);
+  static const Color ffColor = Color(0xffFFFFFF);
+  static const Color f63Color = Color(0xff636363);
+  static const Color afColor = Color(0xffAFAFAF);
+  static Color ff3Color = const Color(0xffFFFFFF).withOpacity(0.3);
+  static const Color f58Color = Color(0xff585858);
+  static const Color e9Color = Color(0xffE9E9E9);
+  static const Color f00Color = Color(0xffE1D800);
+  static const Color d9Color = Color(0xffD9D9D9);
+  static const Color f7Color = Color(0xffF7F7F7);
+  static Color a02Color = const Color(0xff008FA0).withOpacity(0.2);
+  static const Color f32Color = Color(0xff323232);
+  static Color ff4Color = const Color(0xffFFFFFF).withOpacity(0.4);
+  static Color a004Color = const Color(0xff008FA0).withOpacity(0.04);
+  static const Color a2Color = Color(0xffA2A2A2);
+  static const Color d3dColor = Color(0xff3D3D3D);
+  static const Color f96Color = Color(0xff399796);
+  static const Color f33Color = Color(0xffFFD233);
+  static Color f006Color = const Color(0xff000000).withOpacity(0.6);
+  static const Color f8eColor = Color(0xff8E8E8E);
+  static const Color a0Colorlinear1 = Color(0xff008FA0);
+  static const Color cdColorlinear2 = Color(0xff00CFCD);
+  static Color a01Color = const Color(0xff008FA0).withOpacity(0.1);
+  static Color f1a6Color = const Color(0xffE07B1A).withOpacity(0.6);
+}
---
+++ lib/utils/styleimage.dart
@@ -0,0 +1,59 @@
+class StyleImageApp {
+  StyleImageApp._();
+
+  static const String styleImageAvter = "assets/image/avter.svg";
+
+  static const String styleImageAvter2 = "assets/image/avter2.svg";
+
+  static const String styleImageAvter3 = "assets/image/avter3.svg";
+
+  static const String styleImageAvter4 = "assets/image/avter4.svg";
+
+  static const String styleImageAvter5 = "assets/image/avter5.svg";
+
+  static const String styleImageAvter6 = "assets/image/avter6.svg";
+
+  static const String styleImageAvter7 = "assets/image/avter7.svg";
+
+  static const String styleImageAvter8 = "assets/image/avter8.svg";
+
+  static const String styleImageAvterMap = "assets/image/avter_map.svg";
+
+  static const String styleImageAvterMap2 = "assets/image/avter_map2.svg";
+
+  static const String styleImageAvterMap3 = "assets/image/avter_map3.svg";
+
+  static const String styleImageCategory = "assets/image/category.svg";
+
+  static const String styleImageCategory2 = "assets/image/category2.svg";
+
+  static const String styleImageCategory3 = "assets/image/category3.svg";
+
+  static const String styleImageCategory4 = "assets/image/category4.svg";
+
+  static const String styleImageEarth = "assets/image/earth.svg";
+
+  static const String styleImageEarth2 = "assets/image/earth2.svg";
+
+  static const String styleImageGroupImage = "assets/image/group_image.svg";
+
+  static const String styleImageGroupTrip = "assets/image/group_trip.svg";
+
+  static const String styleImageImagePlace = "assets/image/image_place.svg";
+
+  static const String styleImageImagePlace2 = "assets/image/image_place2.svg";
+
+  static const String styleImageImagePlace3 = "assets/image/image_place3.svg";
+
+  static const String styleImageImagePlace4 = "assets/image/image_place4.svg";
+
+  static const String styleImageImagePlace5 = "assets/image/image_place5.svg";
+
+  static const String styleImageLogoEarth = "assets/image/logo_earth.svg";
+
+  static const String styleImageSocial = "assets/image/social.svg";
+
+  static const String styleImageSocial1 = "assets/image/social1.svg";
+
+  static const String styleImageSocial3 = "assets/image/social3.svg";
+}
